f8796350de8be020d521966d0d95aa9e318d27b1
joel garcia <juelgarcia98>
2020-06-02 12:37:51 -0500

    Subida de música front y firestore

--- src/components/Songs/AddSongForm/AddSongForm.js
+++ src/components/Songs/AddSongForm/AddSongForm.js
@@ -1,6 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Form, Button, Input, Icon, Dropdown } from "semantic-ui-react";
+import { useDropzone } from "react-dropzone";
 import { map } from "lodash";
+import { toast } from "react-toastify";
+import { v4 as uuidv4 } from "uuid";
 import firebase from "../../../utils/Firebase";
 import "firebase/firestore";
 import "firebase/storage";
@@ -9,37 +12,12 @@ import "./AddSongForm.scss";
 
 const db = firebase.firestore(firebase);
 
-const countryOptions = [
-  { key: "af", value: "af", flag: "af", text: "Afghanistan" },
-  { key: "ax", value: "ax", flag: "ax", text: "Aland Islands" },
-  { key: "al", value: "al", flag: "al", text: "Albania" },
-  { key: "dz", value: "dz", flag: "dz", text: "Algeria" },
-  { key: "as", value: "as", flag: "as", text: "American Samoa" },
-  { key: "ad", value: "ad", flag: "ad", text: "Andorra" },
-  { key: "ao", value: "ao", flag: "ao", text: "Angola" },
-  { key: "ai", value: "ai", flag: "ai", text: "Anguilla" },
-];
-
 export default function AddSongForm(props) {
   const { setShowModal } = props;
+  const [formData, setFormData] = useState(initialValueForm());
   const [albums, setAlbums] = useState([]);
-  const [tinyBanner, setTinyBanner] = useState(null);
-  //console.log(tinyBanner);
-
-  let imgAlbum = (album) => {
-    let a;
-    firebase
-      .storage()
-      .ref(`album/${album.banner}`)
-      .getDownloadURL()
-      .then((url) => {
-        a = url;
-        setTinyBanner(a);
-        console.log(a);
-      });
-
-    return a;
-  };
+  const [file, setFile] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     db.collection("albums")
@@ -70,14 +48,75 @@ export default function AddSongForm(props) {
       });
   }, []);
 
+  const onDrop = useCallback((acceptedFile) => {
+    const file = acceptedFile[0];
+    setFile(file);
+    //console.log(acceptedFile);
+  });
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: ".mp3",
+    noKeyboard: true,
+    onDrop,
+  });
+
+  const uploadSong = (fileName) => {
+    const ref = firebase.storage().ref().child(`song/${fileName}`);
+    return ref.put(file);
+  };
+
   const onSubmit = () => {
-    console.log("Submit...");
+    if (!formData.name || !formData.album) {
+      toast.warning(
+        "El nombre de la canción y el álbum al que pertenece son obligatorios."
+      );
+    } else if (!file) {
+      toast.warning("La canción es obligatoria.");
+    } else {
+      setIsLoading(true);
+      const fileName = uuidv4();
+      uploadSong(fileName)
+        .then(() => {
+          db.collection("songs")
+            .add({
+              name: formData.name,
+              album: formData.album,
+              fileName: fileName,
+            })
+            .then(() => {
+              toast.success("Canción subida correctamente.");
+              resetForm();
+              setShowModal(false);
+            })
+            .catch(() => {
+              toast.warning("Error al subir canción.");
+            })
+            .finally(() => {
+              setIsLoading(false);
+            });
+        })
+        .catch(() => {
+          toast.error("Error al subir la canción.");
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
+  };
+
+  const resetForm = () => {
+    setFormData(initialValueForm());
+    setFile(null);
+    setAlbums([]);
   };
 
   return (
     <Form className="add-song-form" onSubmit={onSubmit}>
       <Form.Field>
-        <Input placeholder="Nombre de la canción" />
+        <Input
+          placeholder="Nombre de la canción"
+          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+        />
       </Form.Field>
       <Form.Field>
         <Dropdown
@@ -86,12 +125,37 @@ export default function AddSongForm(props) {
           selection
           lazyLoad
           options={albums}
+          onChange={(e, data) => {
+            setFormData({ ...formData, album: data.value });
+          }}
         />
       </Form.Field>
       <Form.Field>
-        <h2>Upload Image</h2>
+        <div className="song-upload" {...getRootProps()}>
+          <input {...getInputProps()} />
+          <Icon name="cloud upload" className={file && "load"} />
+          <div>
+            <p>
+              Arrastra tu canción o haz click <span>aquí</span>.
+            </p>
+            {file && (
+              <p>
+                Canción subida: <span>{file.name}</span>
+              </p>
+            )}
+          </div>
+        </div>
       </Form.Field>
-      <Button type="submit">Subir canción</Button>
+      <Button type="submit" loading={isLoading}>
+        Subir canción
+      </Button>
     </Form>
   );
+}
+
+function initialValueForm() {
+  return {
+    name: "",
+    album: "",
+  };
 }
